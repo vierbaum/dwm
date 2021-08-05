86a476037f8438017b3155f4c19c7a5659a557d0
nld <nld@pad.localdomain>
2021-08-05 09:18:29 +0200

    fullscreen patch

--- config.h
+++ config.h
@@ -76,6 +76,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_plus,   setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_0,      setgaps,        {.i = 0  } },
+	{ MODKEY,                       XK_f,      togglefullscr,  {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
