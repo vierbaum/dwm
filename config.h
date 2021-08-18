ef92b781d5bd50addaea4c46c3a2b27d21a9fbab
nld <nld@pad.localdomain>
2021-08-18 11:40:40 +0200

    activeindicatorbar + cutom size

--- config.h
+++ config.h
@@ -75,11 +75,6 @@ static const Layout layouts[] = {
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
-/* commands */
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
-
 /*First arg only serves to match against key in rules*/
 static const char *scratchpadcmd1[]  = {"a", "st", "-t", "scratchpad1", NULL};
 static const char *scratchpadcmd2[]  = {"b", "st", "-t", "scratchpad2", NULL};
